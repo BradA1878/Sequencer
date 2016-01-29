ff4f892d3500b3bf475aa406f97fe5e2f781e908
Brad Anderson <banderson@terradatum.com>
2016-01-29 15:59:51 -0700

    [Sequencer] #comment Optimize sequence class

---
+++ src/sequencer/sequencer.js
@@ -0,0 +1,29 @@
+var sequence = function() {
+    var _s = function() {
+        var _i = this, _a = [], _end = null, _next = function() {
+            if (_a.length) {
+                var o = _a.shift();
+                o.args ? o.func.apply(o.scope, o.args.concat([o.callback])) : o.func(o.callback);
+            } else {
+                _end && (_end.args ? _end.func.apply(_end.scope, _end.args) : _end.func()), _a.splice(0), _i = _next = _end = _a = null;
+            }
+        };
+        this.start = function(f, a, s) {
+            this.next(f, a, s);
+            return _i;
+        };
+        this.next = function(f, a, s) {
+            _a.push({func:f, args:a, scope:s, callback:function() {
+                _next();
+            }});
+            return _i;
+        };
+        this.finish = function(f, a, s) {
+            _end = {func:f, args:a, scope:s};
+            _next();
+        };
+    };
+    return {start:function(f, a, s) {
+        return (new _s).start(f, a, s);
+    }};
+}();
